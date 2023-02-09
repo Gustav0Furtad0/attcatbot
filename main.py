9120c9217a0404ee78a50bc894ffd84731469c86
Gustav0Furtad0 <gufurtado02@gmail.com>
2023-02-09 08:51:56 -0300

    prevencao de erros usuario

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
-import openpyxl as opx
 import scraping
+import os
+import openpyxl as opx
 
-print("\n\n\n------- Arquivo prefeitura -----")
-filePrefeitura = {
-    "fName": input("Digite o nome do arquivo: "),
-    "ixTabela": int(input("Digite o índice da tablea: ")),
-    "colunaCodCliente": int(input("Digite o índice da coluna que contenha o código do cliente: ")),
-    "colunaNomeCliente": int(input("Digite o índice da coluna que contenha o nome do cliente: ")),
-    "colunaQuantidade": int(input("Digite o índice da coluna que contenha a quantidade: ")),
-    "colunaUnidade": int(input("Digite o índice da coluna que contenha a unidade do item: "))
-}
+filePrefeitura = {}
+fileBranet = ""
 
-print("\n\n\n------- Arquivo Branet -----")
-fileBranet = input("Digite o nome do arquivo: ")
+def innerData():
+    print("\n\n\n------- Arquivo prefeitura -----")
+    filePrefeitura = {
+        "fName": input("Digite o nome do arquivo: "),
+        "ixTabela": int(input("Digite o índice da tablea: ")),
+        "colunaCodCliente": int(input("Digite o índice da coluna que contenha o código do cliente: ")),
+        "colunaNomeCliente": int(input("Digite o índice da coluna que contenha o nome do cliente: ")),
+        "colunaQuantidade": int(input("Digite o índice da coluna que contenha a quantidade: ")),
+        "colunaUnidade": int(input("Digite o índice da coluna que contenha a unidade do item: "))
+    }
+
+    print("\n\n\n------- Arquivo Branet -----")
+    fileBranet = input("Digite o nome do arquivo: ")
+    
+while True:
+    innerData()
+    if os.path.isfile(filePrefeitura["fName"]) and os.path.isfile(fileBranet):
+        break
+    else:
+        print("Arquivo não encontrado, tente novamente...")
+        
+    catologo = opx.load_workbook(filename=filePrefeitura["fName"])
+    tabelaEscolha = catologo[catologo.sheetnames[filePrefeitura["ixtable"]]]
+    nomeUnidade = tabelaEscolha.cell(row=2, column=filePrefeitura["colunaCodCliente"]).value
+    
+    if input("A unidade que deseja alterar o catálogo é " + nomeUnidade + "? (y/n)") == "y":
+        break
+    else:
+        print('Por favor, digite novamente os dados...')
+        
+
 
 navigator = scraping.ChromeDriver()
 
